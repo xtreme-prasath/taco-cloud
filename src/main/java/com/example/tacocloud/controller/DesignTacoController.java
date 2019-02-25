29f2899ca0165bf6aa1cdde8a23c68e84e3d4084
Prasath <dvprasath@gmail.com>
2019-02-24 22:55:28 -0500

    Ch:2 Fuckin hell!
    Added validation to Order & Taco classes.
    Changed the signature of the processOrder to perform validation.

--- src/main/java/com/example/tacocloud/controller/DesignTacoController.java
+++ src/main/java/com/example/tacocloud/controller/DesignTacoController.java
@@ -7,10 +7,12 @@ import domain.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -55,7 +57,12 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco design) {
+    public String processDesign(@Valid Taco design, Errors errors) {
+
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
         // Save the taco design...
         // We'll do this in chapter 3
         log.info("Processing design: " + design);
